457b3657092949ffd1e24e49fcb6679458d4d3e2
thegreatL <kenandrewcarlon1@gmail.com>
2026-03-21 00:30:31 +0800

    use prisma for crud

--- server/src/features/auth/session.repository.ts
+++ server/src/features/auth/session.repository.ts
@@ -1,6 +1,5 @@
-import {Prisma, PrismaClient} from '@prisma/client';
-
-const prisma = new PrismaClient();
+import {Prisma} from '@prisma/client';
+import {prisma} from '../../shared/lib/prisma';
 
 export class SessionRepository {
   async create(data: {userId: string; refreshToken: string; expiresAt: Date}) {
