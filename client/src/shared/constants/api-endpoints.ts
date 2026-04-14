526502c46cb0a80be4b7c73a5f269efe3f93cd3d
thegreatL <kenandrewcarlon1@gmail.com>
2026-04-14 22:19:37 +0800

    remove test code

--- client/src/shared/constants/api-endpoints.ts
+++ client/src/shared/constants/api-endpoints.ts
@@ -4,7 +4,6 @@ const API_ENDPOINTS = {
     REGISTER: '/auth/register',
     REFRESH: '/auth/refresh',
     LOGOUT: '/auth/logout',
-    LOGOUT: '/auth/logout',
     ME: '/auth/me',
     CSRF: '/auth/csrf',
   },
